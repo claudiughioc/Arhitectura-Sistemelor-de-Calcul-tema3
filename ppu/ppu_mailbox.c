22a20f98576d9f5ce54e91bd4197a15027b42a80
Claudiu Ghioc <claudiu.ghioc@gmail.com>
2012-04-25 01:26:43 +0300

    The PPU sends to the SPU a fixed number of patches
    *   The patches are chosen randomly from a pool of
    patches
    
    Signed-off-by: Claudiu Ghioc <claudiu.ghioc@gmail.com>

--- ppu/ppu_mailbox.c
+++ ppu/ppu_mailbox.c
@@ -6,7 +6,8 @@
 #include <stdint.h>
 #include <string.h>
 
-#define SPU_THREADS 8
+#define SPU_THREADS 	8
+#define NUMBER_TRIES	1
 // macro for rounding input value to the next higher multiple of either 
 // 16 or 128 (to fulfill MFC's DMA requirements)
 #define spu_mfc_ceil128(value)  ((value + 127) & ~127)
@@ -64,7 +65,7 @@ void read_from_file(FILE *fin, struct pixel **a, long *width, long *height,
 				if (tok == NULL)
 					break;
 				*height = atol(tok);
-			}
+			};
 			*a = malloc(*width * *height * sizeof(struct pixel));
 			if (!*a) {
 				perror("Error on alocating matrix");
@@ -130,6 +131,18 @@ struct pixel **build_pool_patches(int count, struct pixel *img, struct pixel **p
 }
 
 
+void send_random_patch_to_spu(int spu_no, struct pixel **patches, int pool_size,
+	spe_context_ptr_t *ctxs)
+{
+	struct pixel *curr_patch = get_random_patch(patches, pool_size);
+	printf("PPU is sending patch to SPU %d, pixel%d %d %d\n", spu_no,
+		curr_patch[0].red, curr_patch[0].green, curr_patch[0].blue);
+	int pointer = curr_patch;
+	spe_in_mbox_write(ctxs[spu_no], (void *)&pointer, 1,
+		SPE_MBOX_ANY_NONBLOCKING);
+}
+
+
 
 int main(int argc, char **argv)
 {
@@ -137,13 +150,13 @@ int main(int argc, char **argv)
 	pthread_t threads[SPU_THREADS];
 	thread_arg_t arg[SPU_THREADS];
 	spe_event_unit_t pevents[SPU_THREADS],
-			 events_received[SPU_THREADS];
+			 event_received;
 	spe_event_handler_ptr_t event_handler;
 	event_handler = spe_event_handler_create();
 
 	int zoom, rows, columns, overlap_vert, overlap_oriz, pos_patch;
 	long height, width, i, j, k, petic_x, petic_y;
-	int max_color, patch_w, patch_h, pool_size;
+	int max_color, patch_w, patch_h, pool_size, nevents;
 	struct pixel *a = NULL, *result = NULL;
 	struct pixel **patches __attribute__ ((aligned(16))) = NULL;
 
@@ -234,7 +247,7 @@ int main(int argc, char **argv)
 		spe_in_mbox_write(ctxs[i], (void *) &patch_w, 1,
 			SPE_MBOX_ANY_NONBLOCKING);
 		first_patch = get_random_patch(patches, pool_size);
-		printf("PPU sends to SPU %d: %d, %d, %d\n", i, 
+		printf("PPU sends to SPU %ld: %d, %d, %d\n", i, 
 			first_patch[0].red, first_patch[0].green,
 			first_patch[0].blue);
 		int pointer = (int)first_patch;
@@ -244,26 +257,31 @@ int main(int argc, char **argv)
 	}
 
 
-	/* Getting the offset of LS in data buffer
-	unsigned int data_pointer;
-	int ret=0,done=0;
-	for (;done<8; ret = spe_event_wait(event_handler, events_received,
-				1, 0)) {
-		// do useful stuff
-		if (ret == 0)
+
+	/* Send random patches to fill a column in SPU */
+	int repeats = (rows - 1) * NUMBER_TRIES * SPU_THREADS, curr_spu_no = -1;
+	for (i = 0; i < repeats; i++) {
+		printf("\n\n\nPPU repeat %ld, waiting for event...\n", i);
+		nevents = spe_event_wait(event_handler, &event_received, 1, -1);
+
+		/* Processing */
+		if (curr_spu_no != -1)
+			send_random_patch_to_spu(curr_spu_no, patches, pool_size,
+				ctxs);
+
+		if (nevents <= 0)
 			continue;
-		if (ret<0) {
-			printf("Error: event wait error\n");
-			continue;
-		}
-		if (events_received[0].events & SPE_EVENT_OUT_INTR_MBOX) {
-			printf("SPU%d sent me a message\n",events_received[0].data.u32);
-			// read the mailbox for the spe that issued the event
-			spe_out_intr_mbox_read(events_received[0].spe, (unsigned int*)&data_pointer, 1, SPE_MBOX_ANY_BLOCKING);
-			printf("SPU%d says: %d\n", events_received[0].data.u32, data_pointer);
-			done += 1;
-		}
-	}*/
+		while(spe_out_intr_mbox_status(event_received.spe) < 1);
+		spe_out_intr_mbox_read(event_received.spe, &curr_spu_no, 1,
+			SPE_MBOX_ANY_NONBLOCKING);
+		printf("PPU am primit request de la SPU %d\n", curr_spu_no);
+	}
+	/* Processing the last requesti */
+	if (curr_spu_no != -1)
+			send_random_patch_to_spu(curr_spu_no, patches, pool_size,
+							ctxs);
+
+
 
 	/* Wait for SPU-thread to complete execution. */
 	for (i = 0; i < SPU_THREADS; i++) {
--- spu/spu_mailbox.c
+++ spu/spu_mailbox.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <spu_mfcio.h>
+#include <string.h>
 
 #define waitag(t) mfc_write_tag_mask(1<<t); mfc_read_tag_status_all();
-#define BUFFER_SIZE 3072
+#define BUFFER_SIZE 	3072
+#define NUMBER_TRIES	1
 
 struct pixel {
 	char red;
@@ -10,13 +12,14 @@ struct pixel {
 	char blue;
 };
 
-struct pixel *patch __attribute__ ((alligned(16)));;
+struct pixel *patch __attribute__ ((alligned(16)));
+struct pixel *zone __attribute__ ((alligned(16)));
 
 volatile char buffer[BUFFER_SIZE] __attribute__ ((aligned(128)));
 
 int main(unsigned long long speid, unsigned long long argp, unsigned long long envp){
-	printf("Start SPU %lld\n", argp);
-	int pointer_patch, rows, patch_h, patch_w;
+	printf("\tStart SPU %lld\n", argp);
+	int pointer_patch, rows, patch_h, patch_w, i, j, k;
 
 	/* Wait for initial parameters and the first patch from SPU */
 	while(spu_stat_in_mbox() <= 0);
@@ -27,27 +30,57 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
 	patch_w = spu_read_in_mbox();
 	//printf("SPU %lld am primit prin mailbox: rows %d, ph %d, pw%d\n", argp,
 	//	rows, patch_h, patch_w);
+	/* Allocate memory for patch and zone */
 	patch = malloc(patch_h * patch_w * sizeof(struct pixel));
+	zone = malloc(rows * patch_h * patch_w * sizeof(struct pixel));
 	while(spu_stat_in_mbox() <= 0);
 	pointer_patch = spu_read_in_mbox();
 	//printf("SPU %lld primeste pointer %d\n", argp, pointer_patch);
 
 
-	printf("SPU %lld gets before reading patch\n", argp);
 	/* Read the first patch */
 	uint32_t tag_id = mfc_tag_reserve();
 	if (tag_id == MFC_TAG_INVALID) {
-		printf("Cannot allocate tag id\n");
+		printf("\tSPU Cannot allocate tag id\n");
 		return -1;
 	}
 	mfc_get((void *)(patch), (void *)pointer_patch,
 		(uint32_t) patch_h * patch_w * sizeof(struct pixel), tag_id, 0, 0);
 	waitag(tag_id);
-	printf("SPU %lld primeste pixel: %d, %d, %d\n", argp, patch[0].red,
-		patch[0].green, patch[0].blue);
+	
+	/* Attach the first patch */
+	memcpy((void *)zone, (const void *)patch,
+		patch_h * patch_w * sizeof(struct pixel));
 
 
+
+	/* Send requests to receive the rest of the patches */
+	int repeats = (rows - 1) * NUMBER_TRIES;
+	for (i = 0; i < repeats; i++) {
+		printf("\tSPU %lld trimite request nr %d\n", argp, i);
+		spu_write_out_intr_mbox((uint32_t)argp);
+
+		/* Waiting for the pointer to the next patch */
+		while(spu_stat_in_mbox() <= 0);
+		pointer_patch = spu_read_in_mbox();
+		
+		/* DMA transfer */
+		tag_id = mfc_tag_reserve();
+		if (tag_id == MFC_TAG_INVALID) {
+				printf("Cannot allocate tag id\n");
+				return -1;
+		}
+		mfc_get((void *)(patch), (void *)pointer_patch,
+						(uint32_t) patch_h * patch_w * sizeof(struct pixel), tag_id, 0, 0);
+		waitag(tag_id);
+		printf("\tSPU %lld primeste patch nr %d, pixel %d %d %d\n", argp, i,
+			patch[0].red, patch[0].green, patch[0].blue);
+	}
+
+
+
+
+	/* Send the final zone to the PPU */
 
 	return 0;
 }
-
